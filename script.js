158b7bb5650ea904f6792e464412f5c987f5f551
Taverniers Dylan <252625083+Vel-Daeryn@users.noreply.github.com>
2026-04-04 18:56:24 +0200

    [feat] Home page complete
    Home page completed
    -- Need to verify bugs
    -- Need to verify CSS properties

--- script.js
+++ script.js
@@ -31,4 +31,49 @@ buttonMenu.addEventListener("click", function(e) {
             navigation.classList.remove("closed")
         }, 1000);
     }
-}) 
+})
+
+/* 
+Carousel of Customers Survey
+*/
+
+const survey = document.querySelectorAll(".customer-answer")
+const carouselButtons = document.querySelectorAll("#customer-survey > .button-container > button")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+let surveyIndex = 0;
+
+console.log(carouselButtons);
+
+carouselButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
+
+        if(e.target.id === "prev") {
+
+            if(surveyIndex === 0) {
+                survey[surveyIndex].classList.remove("displayed")
+                surveyIndex += carouselButtons.length
+                survey[surveyIndex].classList.add("displayed")
+            } else {
+                survey[surveyIndex].classList.remove("displayed")
+                surveyIndex -= 1
+                survey[surveyIndex].classList.add("displayed")
+            }   
+        }
+        
+        if(e.target.id === "next") {
+
+            if(surveyIndex === survey.length - 1) {
+                survey[surveyIndex].classList.remove("displayed")
+                surveyIndex = 0
+                survey[surveyIndex].classList.add("displayed")
+            } else {
+                survey[surveyIndex].classList.remove("displayed")
+                surveyIndex += 1
+                survey[surveyIndex].classList.add("displayed")
+            }
+        }
+        
+    })
+})
